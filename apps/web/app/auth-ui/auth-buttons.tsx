050a445e31fd7c99bfd8e0464f444202fc582dde
Andy Eskridge <andy.eskridge@gmail.com>
2024-07-30 20:57:17 -0500

    chore: Update login button to use Github instead of Google

--- apps/web/app/auth-ui/auth-buttons.tsx
+++ apps/web/app/auth-ui/auth-buttons.tsx
@@ -6,8 +6,8 @@ import { signIn } from "../server/auth";
 
 function SignIn() {
 	return (
-		<Button className="mt-4" onClick={async () => await signIn("google")}>
-			Login with Google
+		<Button className="mt-4" onClick={async () => await signIn("github")}>
+			Login with Github
 		</Button>
 	);
 }
